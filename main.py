3d55fc50433242a4772d604724809f1f76a4bf9e
Junyoung Choi <pantomkid@naver.com>
2023-05-05 11:57:53 +0900

    else 추가작성

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import requests, json
 from sens.sens import send_sms_area1, send_sms_area2, send_sms_area3, url, uri, header
 
 app = FastAPI()
-droen = "10"
 
 patrol_status = "PATROL_STANDBY"
 warning_status = "WARNING_STANDBY"
@@ -35,26 +34,18 @@ async def read_drone_state(drone_id: str, state: Union[int, None] = None):
 
 @app.get("/send_msg")
 async def send_msg(state: Union[int, None]=None):
-    if state == 0:
-        ditto = send_sms_area1.data
-    if state == 1:
-        ditto = send_sms_area2.data
-    if state == 2:
-        ditto = send_sms_area3.data
-    requests.post(url+uri, headers=header, data = json.dumps(ditto))
-    return ditto
-
-
-@app.get("/drone/recon")
-async def sen(state:str = Query(None)):
-    global droen
-    if state == "1":
-        droen = "10"
-
-    if state == "2":
-        droen = "20"
-    
-    return droen
+    if state == 0 or state == 1 or state == 2:
+        if state == 0:
+            ditto = send_sms_area1.data
+        if state == 1:
+            ditto = send_sms_area2.data
+        if state == 2:
+            ditto = send_sms_area3.data
+        requests.post(url+uri, headers=header, data = json.dumps(ditto))
+        send_message = "관리자에게 메시지를 전송을 성공하였습니다."
+    else:
+        send_message = "관리자에게 메시지 전송을 실패하였습니다."
+    return send_message
 
 # DRONE 상태 변경 API
 @app.get("/PATROL_DRONE")
@@ -72,19 +63,23 @@ async def change_PATROL_status(STATUS: str = Query(None)):
             patrol_status = "PATROL_MOVE2"
         elif STATUS.upper() == "PATROL_MOVE3":
             patrol_status = "PATROL_MOVE3"
-    # 현재 LED 상태 반환
+        else:
+            patrol_status = "다시 입력해주세요."
+
     return {"PATROL_STATUS": patrol_status}
 
 @app.get("/CAMERA")
 async def change_CAMERA_status(STATUS: str = Query(None)):
     global camera_status
     if STATUS is not None:
-        # 파라미터 값이 전달된 경우, patrol상태 변경
+        # 파라미터 값이 전달된 경우, camera상태 변경
         if STATUS.upper() == "DETECT_POINT":
             camera_status = "DETECT_POINT"
         elif STATUS.upper() == "DETECT_NO_POINT":
             camera_status = "DETECT_NO_POINT"    
-    # 현재 LED 상태 반환
+        else:
+            camera_status = "다시 입력해주세요."
+
     return {"DETECTION_STATUS": camera_status}
 
 @app.get("/WARNING_DRONE")
@@ -96,5 +91,7 @@ async def change_WARNING_status(STATUS: str = Query(None)):
             warning_status = "WARNING_STANDBY"
         elif STATUS.upper() == "WARNING_MOVE":
             warning_status = "WARNING_MOVE"
-    # 현재 LED 상태 반환
+        else:
+            warning_status = "다시 입력해주세요."
+
     return {"WARNING_STATUS": warning_status}
