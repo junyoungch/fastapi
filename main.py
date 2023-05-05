45979e351d8dccbab41fd878717efa51e5d033da
Junyoung Choi <pantomkid@naver.com>
2023-05-05 11:30:05 +0900

    status정리

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 import requests, json
@@ -8,10 +8,11 @@ import requests, json
 from sens.sens import send_sms_area1, send_sms_area2, send_sms_area3, url, uri, header
 
 app = FastAPI()
+droen = "10"
 
-class Recon_Drone(BaseModel):
-    name= str("recon")
-    state = int("0")
+patrol_status = "PATROL_STANDBY"
+warning_status = "WARNING_STANDBY"
+camera_status = "NO_DETECTION"
 
 @app.get("/")
 async def read_root():
@@ -44,10 +45,56 @@ async def send_msg(state: Union[int, None]=None):
     return ditto
 
 
-# @app.get("/drones/recon-drone/{drone_state}")
-# async def read_drone_state(drone_state: int = 0):
-#     if drone_state == 0:
-#         drone = {"드론 쉬고있음"}
-#     if drone_state == 1:
-#         drone = {"드론 날고있음"}
-#     return drone
+@app.get("/drone/recon")
+async def sen(state:str = Query(None)):
+    global droen
+    if state == "1":
+        droen = "10"
+
+    if state == "2":
+        droen = "20"
+    
+    return droen
+
+# DRONE 상태 변경 API
+@app.get("/PATROL_DRONE")
+async def change_PATROL_status(STATUS: str = Query(None)):
+    global patrol_status
+    if STATUS is not None:
+        # 파라미터 값이 전달된 경우, patrol상태 변경
+        if STATUS.upper() == "PATROL_STANDBY":
+            patrol_status = "PATROL_STANDBY"
+        elif STATUS.upper() == "PATROL_TAKEOFF":
+            patrol_status = "PATROL_TAKEOFF"    
+        elif STATUS.upper() == "PATROL_MOVE1":
+            patrol_status = "PATROL_MOVE1"
+        elif STATUS.upper() == "PATROL_MOVE2":
+            patrol_status = "PATROL_MOVE2"
+        elif STATUS.upper() == "PATROL_MOVE3":
+            patrol_status = "PATROL_MOVE3"
+    # 현재 LED 상태 반환
+    return {"PATROL_STATUS": patrol_status}
+
+@app.get("/CAMERA")
+async def change_CAMERA_status(STATUS: str = Query(None)):
+    global camera_status
+    if STATUS is not None:
+        # 파라미터 값이 전달된 경우, patrol상태 변경
+        if STATUS.upper() == "DETECT_POINT":
+            camera_status = "DETECT_POINT"
+        elif STATUS.upper() == "DETECT_NO_POINT":
+            camera_status = "DETECT_NO_POINT"    
+    # 현재 LED 상태 반환
+    return {"DETECTION_STATUS": camera_status}
+
+@app.get("/WARNING_DRONE")
+async def change_WARNING_status(STATUS: str = Query(None)):
+    global warning_status
+    if STATUS is not None:
+        # 파라미터 값이 전달된 경우, warning 상태 변경
+        if STATUS.upper() == "WARNING_STANDBY":
+            warning_status = "WARNING_STANDBY"
+        elif STATUS.upper() == "WARNING_MOVE":
+            warning_status = "WARNING_MOVE"
+    # 현재 LED 상태 반환
+    return {"WARNING_STATUS": warning_status}
