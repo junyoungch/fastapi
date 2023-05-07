e0114d5964fdafed8b23df752e39e5e8b910a3cd
Junyoung Choi <pantomkid@naver.com>
2023-05-07 14:44:46 +0900

    warning_status 코드 추가

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ app = FastAPI()
 class StateInfo:
     patrol_status = "PATROL_STANDBY"
     warning_status = "WARNING_STANDBY"
-    camera_status = "NO_DETECTION"
+    detect_status = "DETECT_NO_POINT1"
+    gps_coordinate = "0"
 
 @app.get("/")
 async def read_root():
     return {"Welcome to gabozaing"}
 
-# DRONE 상태 변경 API
-@app.get("/StateInfo/PATROL_DRONE")
-async def change_PATROL_status(STATUS: str = Query(None), GPS: str = Query(None)):
+# PATROL_DRONE 상태 변경 API
+@app.get("/StateInfo/PATROL_STATUS")
+async def change_PATROL_status(STATUS: str = Query(None)):
     global StateInfo
-    coordinate = "0"
     if STATUS is not None:
         # 파라미터 값이 전달된 경우, patrol상태 변경
         if STATUS.upper() == "PATROL_STANDBY":
@@ -35,29 +35,18 @@ async def change_PATROL_status(STATUS: str = Query(None), GPS: str = Query(None)
             StateInfo.patrol_status = "PATROL_MOVE2"
         elif STATUS.upper() == "PATROL_MOVE3":
             StateInfo.patrol_status = "PATROL_MOVE3"
+        elif STATUS.upper() == "PATROL_DETECTED":
+            StateInfo.patrol_status = "PATROL_DETECTED"
+        elif STATUS.upper() == "PATROL_RETURN":
+            StateInfo.patrol_status = "PATROL_RETURN"
+            
         else:
             StateInfo.patrol_status = "다시 입력해주세요."
 
-    if GPS == "point1":
-        coordinate = "123.815.165"
+    return {"PATROL_STATUS": StateInfo.patrol_status}
 
-    return {"PATROL_STATUS": StateInfo.patrol_status, "GPS좌표": coordinate}
-
-@app.get("/StateInfo/CAMERA")
-async def change_CAMERA_status(STATUS: str = Query(None)):
-    global StateInfo
-    if STATUS is not None:
-        # 파라미터 값이 전달된 경우, camera상태 변경
-        if STATUS.upper() == "DETECT_POINT":
-            StateInfo.camera_status = "DETECT_POINT"
-        elif STATUS.upper() == "DETECT_NO_POINT":
-            StateInfo.camera_status = "DETECT_NO_POINT"    
-        else:
-            StateInfo.camera_status = "다시 입력해주세요."
-
-    return {"DETECTION_STATUS": StateInfo.camera_status}
-
-@app.get("/StateInfo/WARNING_DRONE")
+# WARNING_DRONE 상태변경
+@app.get("/StateInfo/WARNING_STATUS")
 async def change_WARNING_status(STATUS: str = Query(None)):
     global warning_status
     if STATUS is not None:
@@ -66,11 +55,41 @@ async def change_WARNING_status(STATUS: str = Query(None)):
             StateInfo.warning_status = "WARNING_STANDBY"
         elif STATUS.upper() == "WARNING_MOVE":
             StateInfo.warning_status = "WARNING_MOVE"
+        elif STATUS.upper() == "WARNING_ALARM":
+            StateInfo.warning_status = "WARNING_ALARM"
+        elif STATUS.upper() == "WARNING_RETURN":
+            StateInfo.warning_status = "WARNING_RETURN"
         else:
             StateInfo.warning_status = "다시 입력해주세요."
 
     return {"WARNING_STATUS": StateInfo.warning_status}
 
+# 
+@app.get("/StateInfo/DETECT_STATUS")
+async def change_CAMERA_status(STATUS: str = Query(None)):
+    global StateInfo
+    if STATUS is not None:
+        # 파라미터 값이 전달된 경우, camera상태 변경
+        if STATUS.upper() == "DETECT_POINT1":
+            StateInfo.detect_status = "DETECT_POINT1"
+        elif STATUS.upper() == "DETECT_POINT2":
+            StateInfo.detect_status = "DETECT_POINT2"
+        elif STATUS.upper() == "DETECT_NO_POINT1":
+            StateInfo.camera_status = "DETECT_NO_POINT1"    
+        else:
+            StateInfo.camera_status = "다시 입력해주세요."
+
+    return {"DETECTION_STATUS": StateInfo.camera_status}
+
+#GPS좌표 받아오기
+@app.get("/StateInfo/GPS_COORDINATE")
+async def get_gps_value(GPS_VALUE: str = Query(None)):
+    global StateInfo
+    if GPS_VALUE is not None:
+        StateInfo.gps_coordinate = GPS_VALUE
+
+    return {"GPS_VALUE":StateInfo.gps_coordinate}
+    
 # 관리자에게 메시지 전송
 @app.get("/send_msg")
 async def send_msg(state: Union[int, None]=None):
